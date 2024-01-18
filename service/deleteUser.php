ee99e8cabfb80f60ae1eb3c26ee26d73adb2c6a5
Christian Gonzalez <christian.gonzalez@garsalabs.com>
2024-01-18 12:45:45 -0600

    fix delete user

--- service/deleteUser.php
+++ service/deleteUser.php
@@ -6,20 +6,20 @@ include "utils/mysql.php";
 deleteUser();
 
 function deleteUser(){
-    if (isset($_POST['email']) && isset($_POST['pass']) && isset($_POST['idUser'])) {
+    if (isset($_POST['email']) && isset($_POST['pass']) && isset($_POST['iduser'])) {
 
         $email =  $_POST['email'];
 		$email = urldecode($email);
 	    $pass = $_POST['pass'];
-        $idUser = $_POST['idUser'];
+        $iduser = $_POST['iduser'];
 		$usuario = null;
 
-        if ($email != null && $pass != null && $idUser != null) {
+        if ($email != null && $pass != null && $iduser != null) {
             $db = new MysqlCon();
 			 $db->conectar();
 			 $loginConsulta = $db->consulta(UtilBd::login($email,$pass));
              if($db->num_rows($loginConsulta)>0){
-                $isDeleteUser = $db->bConsulta(UtilBd::deleteUserAccount($idUser));
+                $isDeleteUser = $db->bConsulta(UtilBd::deleteUserAccount($iduser));
                 if ($isDeleteUser) {
                     $data = array('estatus' => '200', 'error' => "Se ha eliminado tu cuenta");
                     $json = json_encode($data, JSON_PRETTY_PRINT);
